7b7eb08c7f664f1292077d2a30ed1ce9347800a2
OZON Pedagog <OZON Pedagog@DESKTOP-TENT9N6>
2024-01-08 14:16:40 +0500

    Добавьте файлы проекта.

---
+++ Lesson_5._Task_3.py
@@ -0,0 +1,17 @@
+﻿print ('Минимальная сумма инвестиций:')
+MIA = int (input () )
+print ('Сколько внёс Майкл:')
+Michael = int (input () )
+print ('Сколько внёс Иван:')
+Ivan = int (input () )
+if (Michael >= MIA) and (Ivan >= MIA):
+    print (2)
+elif (Michael >= MIA) or (Ivan >= MIA):
+    if Michael >= MIA:
+        print ('Mike')
+    else:
+        print ('Ivan')
+elif Michael + Ivan >= MIA:
+    print (1)
+else:
+    print (0)
